c8397394df18cea108b00b551132fdf45dd14211
Irina Antonyuk <ofelia.lamensky@gmail.com>
2026-07-12 20:43:32 +0100

    Fix property names in gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -3,62 +3,62 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
   {
-    small: 'https://picsum.photos/id/10/300/200',
-    large: 'https://picsum.photos/id/10/900/600.jpg',
-    alt: 'Nature landscape',
+    preview: 'https://picsum.photos/id/10/300/200',
+    original: 'https://picsum.photos/id/10/900/600.jpg',
+    description: 'Nature landscape',
   },
   {
-    small: 'https://picsum.photos/id/20/300/200',
-    large: 'https://picsum.photos/id/20/900/600.jpg',
-    alt: 'City street',
+    preview: 'https://picsum.photos/id/20/300/200',
+    original: 'https://picsum.photos/id/20/900/600.jpg',
+    description: 'City street',
   },
   {
-    small: 'https://picsum.photos/id/30/300/200',
-    large: 'https://picsum.photos/id/30/900/600.jpg',
-    alt: 'Mountain view',
+    preview: 'https://picsum.photos/id/30/300/200',
+    original: 'https://picsum.photos/id/30/900/600.jpg',
+    description: 'Mountain view',
   },
   {
-    small: 'https://picsum.photos/id/40/300/200',
-    large: 'https://picsum.photos/id/40/900/600.jpg',
-    alt: 'Forest path',
+    preview: 'https://picsum.photos/id/40/300/200',
+    original: 'https://picsum.photos/id/40/900/600.jpg',
+    description: 'Forest path',
   },
   {
-    small: 'https://picsum.photos/id/50/300/200',
-    large: 'https://picsum.photos/id/50/900/600.jpg',
-    alt: 'Ocean waves',
+    preview: 'https://picsum.photos/id/50/300/200',
+    original: 'https://picsum.photos/id/50/900/600.jpg',
+    description: 'Ocean waves',
   },
   {
-    small: 'https://picsum.photos/id/60/300/200',
-    large: 'https://picsum.photos/id/60/900/600.jpg',
-    alt: 'Desert dunes',
+    preview: 'https://picsum.photos/id/60/300/200',
+    original: 'https://picsum.photos/id/60/900/600.jpg',
+    description: 'Desert dunes',
   },
   {
-    small: 'https://picsum.photos/id/70/300/200',
-    large: 'https://picsum.photos/id/70/900/600.jpg',
-    alt: 'Snowy peaks',
+    preview: 'https://picsum.photos/id/70/300/200',
+    original: 'https://picsum.photos/id/70/900/600.jpg',
+    description: 'Snowy peaks',
   },
   {
-    small: 'https://picsum.photos/id/80/300/200',
-    large: 'https://picsum.photos/id/80/900/600.jpg',
-    alt: 'Sunset sky',
+    preview: 'https://picsum.photos/id/80/300/200',
+    original: 'https://picsum.photos/id/80/900/600.jpg',
+    description: 'Sunset sky',
   },
   {
-    small: 'https://picsum.photos/id/90/300/200',
-    large: 'https://picsum.photos/id/90/900/600.jpg',
-    alt: 'Green valley',
+    preview: 'https://picsum.photos/id/90/300/200',
+    original: 'https://picsum.photos/id/90/900/600.jpg',
+    description: 'Green valley',
   },
 ];
 
 const galleryList = document.querySelector('.gallery');
 
-function createGalleryItem({ small, large, alt }) {
+function createGalleryItem({ preview, original, description }) {
   return `
 <li class="gallery-item">
-<a class="gallery-link" href="${large}">
+<a class="gallery-link" href="${original}">
 <img
 class="gallery-image"
-src="${small}"
-alt="${alt}"
+src="${preview}"
+alt="${description}"
 />
 </a>
 </li>
