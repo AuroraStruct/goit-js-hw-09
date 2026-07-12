1bab9fbb53e09ec38ebc072ee2fa17c281bc213b
AuroraStruct <267740499+AuroraStruct@users.noreply.github.com>
2026-07-12 19:43:56 +0000

    Deploying to gh-pages from @ AuroraStruct/goit-js-hw-09@c8397394df18cea108b00b551132fdf45dd14211 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,12 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as l}from"./assets/vendor-CgTBfC_f.js";const a=[{small:"https://picsum.photos/id/10/300/200",large:"https://picsum.photos/id/10/900/600.jpg",alt:"Nature landscape"},{small:"https://picsum.photos/id/20/300/200",large:"https://picsum.photos/id/20/900/600.jpg",alt:"City street"},{small:"https://picsum.photos/id/30/300/200",large:"https://picsum.photos/id/30/900/600.jpg",alt:"Mountain view"},{small:"https://picsum.photos/id/40/300/200",large:"https://picsum.photos/id/40/900/600.jpg",alt:"Forest path"},{small:"https://picsum.photos/id/50/300/200",large:"https://picsum.photos/id/50/900/600.jpg",alt:"Ocean waves"},{small:"https://picsum.photos/id/60/300/200",large:"https://picsum.photos/id/60/900/600.jpg",alt:"Desert dunes"},{small:"https://picsum.photos/id/70/300/200",large:"https://picsum.photos/id/70/900/600.jpg",alt:"Snowy peaks"},{small:"https://picsum.photos/id/80/300/200",large:"https://picsum.photos/id/80/900/600.jpg",alt:"Sunset sky"},{small:"https://picsum.photos/id/90/300/200",large:"https://picsum.photos/id/90/900/600.jpg",alt:"Green valley"}],i=document.querySelector(".gallery");function o({small:t,large:s,alt:p}){return`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as s}from"./assets/vendor-CgTBfC_f.js";const o=[{preview:"https://picsum.photos/id/10/300/200",original:"https://picsum.photos/id/10/900/600.jpg",description:"Nature landscape"},{preview:"https://picsum.photos/id/20/300/200",original:"https://picsum.photos/id/20/900/600.jpg",description:"City street"},{preview:"https://picsum.photos/id/30/300/200",original:"https://picsum.photos/id/30/900/600.jpg",description:"Mountain view"},{preview:"https://picsum.photos/id/40/300/200",original:"https://picsum.photos/id/40/900/600.jpg",description:"Forest path"},{preview:"https://picsum.photos/id/50/300/200",original:"https://picsum.photos/id/50/900/600.jpg",description:"Ocean waves"},{preview:"https://picsum.photos/id/60/300/200",original:"https://picsum.photos/id/60/900/600.jpg",description:"Desert dunes"},{preview:"https://picsum.photos/id/70/300/200",original:"https://picsum.photos/id/70/900/600.jpg",description:"Snowy peaks"},{preview:"https://picsum.photos/id/80/300/200",original:"https://picsum.photos/id/80/900/600.jpg",description:"Sunset sky"},{preview:"https://picsum.photos/id/90/300/200",original:"https://picsum.photos/id/90/900/600.jpg",description:"Green valley"}],e=document.querySelector(".gallery");function r({preview:i,original:t,description:p}){return`
 <li class="gallery-item">
-<a class="gallery-link" href="${s}">
+<a class="gallery-link" href="${t}">
 <img
 class="gallery-image"
-src="${t}"
+src="${i}"
 alt="${p}"
 />
 </a>
 </li>
-`}const e=a.map(o).join("");i.insertAdjacentHTML("beforeend",e);new l(".gallery a",{captionsData:"alt",captionDelay:250});
+`}const a=o.map(r).join("");e.insertAdjacentHTML("beforeend",a);new s(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
